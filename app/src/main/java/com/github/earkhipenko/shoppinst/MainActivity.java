1721831ed7b5645a824ab7b7bdee29f36f9e35de
Evgeny Lyzov <lyzov.e.r@gmail.com>
2016-12-18 12:33:06 +0700

    Added Shopping List items layouts

--- app/src/main/java/com/github/earkhipenko/shoppinst/MainActivity.java
+++ app/src/main/java/com/github/earkhipenko/shoppinst/MainActivity.java
@@ -5,10 +5,56 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView shoppingItems;
+    private RecyclerView.Adapter shoppingItemsAdapter = new RecyclerView.Adapter() {
+        private final int ACTIVE_VIEW=1;
+        private final int INACTIVE_VIEW=2;
+        private final int SUBHEADER_VIEW=3;
+
+        @Override
+        public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            if (viewType == ACTIVE_VIEW) {
+                View v = getLayoutInflater().inflate(R.layout.active_item, parent, false);
+                return new ActiveItemViewHolder(v,
+                        (CheckBox)v.findViewById(R.id.item_status),
+                        (TextView)v.findViewById(R.id.item_name),
+                        (TextView)v.findViewById(R.id.item_quantity),
+                        (ImageView)v.findViewById(R.id.item_action)
+                );
+            } else if (viewType == INACTIVE_VIEW) {
+                View v = getLayoutInflater().inflate(R.layout.inactive_item, parent, false);
+                return new InactiveItemViewHolder(v,
+                        (CheckBox)v.findViewById(R.id.item_status),
+                        (TextView)v.findViewById(R.id.item_name),
+                        (ImageView)v.findViewById(R.id.item_action)
+                );
+            } else {
+                View v = getLayoutInflater().inflate(R.layout.subheader, parent, false);
+                return new SubheaderViewHolder(v);
+            }
+        }
+
+        @Override
+        public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return 0;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +72,9 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
+
+        shoppingItems = (RecyclerView)findViewById(R.id.shopping_items);
+        shoppingItems.setLayoutManager(new LinearLayoutManager(this));
     }
 
 }
---
+++ app/src/main/java/com/github/earkhipenko/shoppinst/SubheaderViewHolder.java
@@ -0,0 +1,14 @@
+package com.github.earkhipenko.shoppinst;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+/**
+ * Created by Evgeny on 18.12.2016.
+ */
+
+public class SubheaderViewHolder extends RecyclerView.ViewHolder {
+    public SubheaderViewHolder(View itemView) {
+        super(itemView);
+    }
+}
